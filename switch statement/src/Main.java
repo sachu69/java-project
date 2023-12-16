989d3520bcbf42d39eea4218bced10e95955d424
sushil_kumar <suahilkumar134@gmail.com>
2023-12-16 21:01:28 +0530

    k

---
+++ switch statement/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String role = "admin";
+        switch (role){
+            case "admin":
+                System.out.println("You're an admin");
+                break;
+            case "moderator":
+                System.out.println("You're an moderator");
+                break;
+            default:
+                System.out.println("Welcome guest");
+        }
+    }
+}
